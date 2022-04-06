806813b235244353381646cf4ab5d82097014e97
Ling-Lang <kulkajens82@gmail.com>
2022-04-06 17:17:34 +0200

    js

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,10 @@
 var canvas = document.getElementById("c_1");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
+ctx.fillStyle = "#2364aa";
 ctx.fillRect(0, 0, 150, 75);
+
+$( document ).on( "mousemove", function( event ) {
+    $( "#log" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+  });
 
 
